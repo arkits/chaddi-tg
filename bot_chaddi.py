817c2e5c028f544746f58a90a79b417e6c6a1495
Archit Khode <arkits@outlook.com>
2019-06-16 23:23:17 -0700

    added messagequeue to avoid flooding

--- bot_chaddi.py
+++ bot_chaddi.py
@@ -6,25 +6,69 @@
 #                        Archit Khode                             #
 ###################################################################
 
+# Telegram bot library specific
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ParseMode
+
+# For messagequeue
+import telegram.bot
+from telegram.ext import messagequeue as mq
+from telegram.utils.request import Request
+
+# Other required stuff
 import logging
 import random
 from datetime import datetime
 
+# Helper utils 
+import config
+import handlers
+
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# Test logging
 logger.info("~~~~ ~~~~ ~~~~ ~~~~")
 logger.info("ChaddiBot")
 logger.info("~~~~ ~~~~ ~~~~ ~~~~")
 
-import config
-import handlers
+
+class ChaddiBot(telegram.bot.Bot):
+
+    # Custom Bot class which delegates send method handling to MQ
+
+    def __init__(self, *args, is_queued_def=True, mqueue=None, **kwargs):
+        super(ChaddiBot, self).__init__(*args, **kwargs)
+
+        # Below 2 attributes should be provided for decorator usage
+        self._is_messages_queued_default = is_queued_def
+        self._msg_queue = mqueue or mq.MessageQueue()
+
+    def __del__(self):
+        try:
+            self._msg_queue.stop()
+        except:
+            pass
+        super(ChaddiBot, self).__del__()
+
+    @mq.queuedmessage
+    def send_message(self, *args, **kwargs):
+        return super(ChaddiBot, self).send_message(*args, **kwargs)
+
 
 def main():
+
+    q = mq.MessageQueue(all_burst_limit=3, all_time_limit_ms=3000)
+
+    token = config.tg_bot_token
+
+    request = Request(con_pool_size=8)
+    chaddiBot = ChaddiBot(token, request=request, mqueue=q)
+    updater = telegram.ext.updater.Updater(bot=chaddiBot)
+
     # Create the EventHandler and pass it your bot's token.
-    updater = Updater(config.tg_bot_token)
+    # updater = Updater(config.tg_bot_token)
 
     # Uncomment the following block for job_spam
     # jq = updater.job_queue
