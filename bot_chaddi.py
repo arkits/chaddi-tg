0a2fff8cd90234b1b8c3972605d0f2883aa0d848
Archit Khode <arkits@outlook.com>
2019-06-16 22:59:32 -0700

    updated logging

--- bot_chaddi.py
+++ bot_chaddi.py
@@ -15,9 +15,9 @@ from datetime import datetime
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-logger.info(' -------- ')
-logger.info('ChaddiBot')
-logger.info(' -------- ')
+logger.info("~~~~ ~~~~ ~~~~ ~~~~")
+logger.info("ChaddiBot")
+logger.info("~~~~ ~~~~ ~~~~ ~~~~")
 
 import config
 import handlers
@@ -42,6 +42,7 @@ def main():
     dp.add_handler(CommandHandler("start", handlers.start))
     dp.add_handler(CommandHandler("hi", handlers.hi))
     dp.add_handler(CommandHandler("timesince", handlers.timesince))
+    dp.add_handler(CommandHandler("superpower", handlers.superpower))
 
     # regular messages
     dp.add_handler(MessageHandler(Filters.text, handlers.all_text))
@@ -52,8 +53,8 @@ def main():
 
     # Start the Bot
     updater.start_polling()
-    logger.info('Started Polling...')
-    logger.info(' -------- ')
+    logger.info("Started Polling...")
+    logger.info("~~~~ ~~~~ ~~~~ ~~~~")
     
     updater.idle()
 
