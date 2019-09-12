02f50f0624cbc182448d8f1c71cd71591180fb83
Archit Khode <arkits@outlook.com>
2019-09-11 20:03:45 -0700

    Updated scripts
    - Fixed kill_prod.sh
    - Tailing logs when run_prod.sh

--- kill_prod.sh
+++ kill_prod.sh
@@ -1,1 +1,6 @@
-ps -ef | grep bot_chaddi.py | grep -v grep | awk '{print $2}' | xargs kill
+PID=`ps -eaf | grep bot_chaddi | grep -v grep | awk '{print $2}'`
+
+if [[ "" !=  "$PID" ]]; then
+  echo "killing $PID"
+  kill -9 $PID
+fi
--- run_prod.sh
+++ run_prod.sh
@@ -1,1 +1,2 @@
 nohup python3 bot_chaddi.py > chaddi.logs 2>&1 & 
+tail -f chaddi.logs
