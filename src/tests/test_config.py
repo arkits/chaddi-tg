2b4ed312609933508f79ca1bf5f9388b32272ec3
Archit Khode <arkits@outlook.com>
2019-10-29 18:37:31 -0700

    updated test_config to assert a few more required configs

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -5,3 +5,8 @@ def test_config():
     assert config.bot_username
     assert config.tg_bot_token
     assert config.is_dev is not None
+
+    if config.is_dev is False:
+        assert config.tg_webhook_url 
+
+    assert config.allowed_setters
