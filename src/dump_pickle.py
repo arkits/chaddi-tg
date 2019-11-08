be9d2cac1f3ec3a1817e26c5b7ba308f06037de8
Khode, Archit (ARC-AFH)[UNIVERSITIES SPACE RESEARCH ASSN] <archit.khode@nasa.gov>
2019-11-08 14:27:52 -0800

    added dump_pickle

---
+++ src/dump_pickle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pickle
+import util.logger
+import json
+
+# Enable logging
+logger = util.logger.get_logger(__name__)
+
+def main():
+
+    try:
+        with open('resources/bakchod.pickle', 'rb') as handle:
+            bakchod_dict = pickle.load(handle)
+            logger.info('Loaded Bakchod pickle!')
+    except:
+        logger.info('Bakchod pickle not found...')
+        return
+    
+    for bakchod in bakchod_dict.values():
+        logger.info(bakchod.__dict__)
+
+if __name__ == "__main__":
+    main()
