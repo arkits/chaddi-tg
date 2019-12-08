f7bd64389d1bf3db1e8c413462f7c455b5dcbe12
Archit Khode <arkits@outlook.com>
2019-12-08 13:50:46 -0800

    cleanup tests now that is_dev is retired

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -4,9 +4,4 @@ import config
 def test_config():
     assert config.bot_username
     assert config.tg_bot_token
-    assert config.is_dev is not None
-
-    if config.is_dev is False:
-        assert config.tg_webhook_url 
-
     assert config.allowed_setters
