80d7670f05a617a69a46866167480d20694d4552
Archit Khode <arkits@outlook.com>
2019-12-08 13:26:40 -0800

    handle webms with or without audo + refactor!

--- src/handlers/webm_converter.py
+++ src/handlers/webm_converter.py
@@ -5,6 +5,7 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ParseMode
 from util import bakchod_util
+from util import chaddi_util
 import ffmpeg
 import datetime
 
@@ -21,34 +22,58 @@ def handle(bot, update):
 
     if document.file_name.endswith(".webm"):
 
-        logger.info("Got a webm - " + str(document.file_id))
+        logger.info("webm: Got a webm - " + str(document.file_id))
 
         time_before = datetime.datetime.now()
-        webm_file = bot.get_file(document.file_id)
-        webm_file.download('resources/to_convert.webm')
-        logger.info("Downloaded webm - " + str(document.file_id))
 
-        stream = ffmpeg.input('resources/to_convert.webm')
-        video = stream.video.filter('crop', 'iw-mod(iw,2)', 'ih-mod(ih,2)')
-        audio = stream.audio
-        joined = ffmpeg.concat(video, audio, v=1, a=1).node
-        output = ffmpeg.output(joined[0], joined[1], 'resources/converted.mp4',vcodec='libx265', crf=28, acodec='libvorbis')
-        output = ffmpeg.overwrite_output(output)
-        ffmpeg.run(output)
+        try:
+            # Download the webm file
+            webm_file = bot.get_file(document.file_id)
+            webm_file.download('resources/to_convert.webm')
+            logger.info("Downloaded webm - " + str(document.file_id))
 
-        logger.info("Finished converting webm - " + str(document.file_id))
+            # Conversion via ffmpeg
+            probe = ffmpeg.probe('resources/to_convert.webm')
+            audio_probe = next((stream for stream in probe['streams'] if stream['codec_type'] == 'audio'), None)
 
-        time_after = datetime.datetime.now()
-        difference = time_after - time_before
-        minutes = int(difference.seconds/60)
-        seconds = difference.seconds%60
-        og_from = update.message.from_user
+            stream = ffmpeg.input('resources/to_convert.webm')
+            video = stream.video.filter('crop', 'iw-mod(iw,2)', 'ih-mod(ih,2)')
 
-        if(og_from['username']):
-            og_sender = "@" + og_from['username']
-        elif(og_from['firstname']):
-            og_sender = og_from['firstname']
+            if audio_probe is None:
+                logger.info("webm: audio_stream is none!")
+                output = ffmpeg.output(video, 'resources/converted.mp4', vcodec='libx265', crf=28, acodec='libvorbis')
+                output = ffmpeg.overwrite_output(output)
+                ffmpeg.run(output)
+            else:
+                logger.info("webm: audio_stream is not none!")
+                audio = stream.audio
+                joined = ffmpeg.concat(video, audio, v=1, a=1).node
+                output = ffmpeg.output(joined[0], joined[1], 'resources/converted.mp4', vcodec='libx265', crf=28, acodec='libvorbis')
+                output = ffmpeg.overwrite_output(output)
+                ffmpeg.run(output)
 
-        caption = document.file_name + " uploaded by " + og_sender + " has been converted to MP4 in " + str(minutes) + " minutes " + str(seconds) + " seconds"
+            # Calculate time taken
+            time_after = datetime.datetime.now()
+            difference = time_after - time_before
+            pretty_time_delta = chaddi_util.pretty_time_delta(difference.seconds)
+            logger.info("webm: Finished converting webm=%s , timeTaken=%s", str(document.file_id), pretty_time_delta)
 
-        update.message.reply_video(video=open('resources/converted.mp4', 'rb'), timeout=5000, caption=caption)
+            og_from = update.message.from_user
+            if(og_from['username']):
+                og_sender = "@" + og_from['username']
+            elif(og_from['firstname']):
+                og_sender = og_from['firstname']
+
+            caption = document.file_name + " uploaded by " + og_sender + " has been converted to MP4 in " + str(pretty_time_delta)
+
+            update.message.reply_video(video=open('resources/converted.mp4', 'rb'), timeout=5000, caption=caption)
+            
+        except Exception as e:
+            
+            logger.error("webm: Caught error in webm_converter - %s", e)
+
+            response = "Error occured during WebM conversion - `" + str(e) + "`"
+            update.message.reply_text(
+                text=response, 
+                parse_mode=ParseMode.MARKDOWN
+            )
