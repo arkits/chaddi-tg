09f6c05475fd0f09aa11398dcc1d95ffff14d878
Archit Khode <arkits@outlook.com>
2019-12-31 13:33:55 +0530

    Update help.py

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
+from telegram import ParseMode
+
 
 # Enable logging
 logger = logging.getLogger(__name__)
@@ -9,19 +11,22 @@ logger = logging.getLogger(__name__)
 
 # Handler help
 def handle(bot, update):
-    logger.info("/help: Print help message")
 
-    help_message = "BOT USAGE: '/ + command' \n" \
-                   "Available commands: \n"\
-                   "/hi or /Hi --- Say hi to you \n"\
-                   "/superpower --- return time until New Year \n"\
-                   "/tts --- \n"\
-                   "/ghati --- \n"\
-                   "/chutiya --- \n"\
-                   "/timesince --- Given a user name, return last login time\n"\
-                   "/rokda --- \n"\
-                   "/jyotish --- \n"\
-                   "/mom --- \n"\
-                   "/quotes --- \n" \
-                   "/choose --- Given a list of options separated by comma or newline, return one of them"
-    update.message.reply_text(help_message)
+    logger.info("/help: Printing help message for user='%s' in group='%s'", update.message.from_user['username'], update.message.chat.title)
+
+    help_message =  "🙏🙏🙏 <b>ChaddiBot Help</b> 🙏🙏🙏" \
+                    " \n \n " \
+                    " <i>Available commands:</i> \n" \
+                    " <code>/superpower</code> - Returns the time until New Year 2020. \n" \
+                    " <code>/tts {text}</code> - Converts passed {text} into speech. \n" \
+                    " <code>/ghati {text}</code> - Converts passed {text} into Hindi speech. \n" \
+                    " <code>/jyotish</code> Tries to solve your problems of life and fails everytime. \n" \
+                    " <code>/mom</code> - Insults your mom. \n "  \
+                    " <code>/rokda</code> - Rokda is the currency of #Bakchodi. Shows how much Rokda you have. \n" \
+                    " <code>/gamble</code> - Lets you gamble your Rokda."
+
+
+    update.message.reply_text(
+        text=help_message,
+        parse_mode=ParseMode.HTML
+    )
