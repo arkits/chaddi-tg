7be13904678da61a73c60a2ec37ab6ab33a2736e
Archit Khode <arkits@outlook.com>
2019-12-31 13:12:31 +0530

    re-implementation of censor module

--- src/handlers/censor.py
+++ src/handlers/censor.py
@@ -5,83 +5,83 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ParseMode
 from util import bakchod_util
-import config
+from util import chaddi_util
 
 # Enable logging
 logger = logging.getLogger(__name__)
 
 # Handler censor
 def handle_censor(bot, update):
-    if is_admin(update.message.from_user['id']):
-        if update.message.reply_to_message:
-            response = process_user(update, True)
-            update.message.reply_to_message.reply_text(
-                text=response,
-                parse_mode=ParseMode.HTML
-            )
+    if chaddi_util.is_admin(update.message.from_user['id']):
+
+        bakchod = get_bakchod_from_context(update)
+
+        response = process_user(bakchod, True)
+
+        update.message.reply_text(
+            text=response,
+            parse_mode=ParseMode.HTML
+        )
+
     else:
-        if update.message.reply_to_message:
-            update.message.reply_to_message.reply_text(
-                text="❌ Not allowed censor",
-                parse_mode=ParseMode.HTML
-            )
-        else:
-            logger.info("censoring id='%s' message='%s'",update.message.from_user['id'], update.message.text)
-            bot.delete_message(
-                chat_id=update.message.chat_id,
-                message_id=update.message.message_id
-            )
+        logger.info("censoring id='%s' message='%s'",update.message.from_user['id'], update.message.text)
+        
+        bot.delete_message(
+            chat_id=update.message.chat_id,
+            message_id=update.message.message_id
+        )
 
 # Handler uncensor
 def handle_uncensor(bot, update):
-    if is_admin(update.message.from_user['id']):
-        if update.message.reply_to_message:
-            response = process_user(update, False)
-            update.message.reply_to_message.reply_text(
-                text=response,
-                parse_mode=ParseMode.HTML
-            )
+    if chaddi_util.is_admin(update.message.from_user['id']):
+
+        bakchod = get_bakchod_from_context(update)
+
+        response = process_user(bakchod, False)
+
+        update.message.reply_text(
+            text=response,
+            parse_mode=ParseMode.HTML
+        )
+
     else:
-        if update.message.reply_to_message:
-            update.message.reply_to_message.reply_text(
-                text="❌ Not allowed uncensor",
-                parse_mode=ParseMode.HTML
-            )
-        else:
-            logger.info("censoring id='%s' message='%s'",update.message.from_user['id'], update.message.text)
-            bot.delete_message(
-                chat_id=update.message.chat_id,
-                message_id=update.message.message_id
-            )
+
+        logger.info("censoring id='%s' message='%s'",update.message.from_user['id'], update.message.text)
+
+        bot.delete_message(
+            chat_id=update.message.chat_id,
+            message_id=update.message.message_id
+        )
 
 
-def process_user(update, to_censor):
+def get_bakchod_from_context(update):
 
-    user_id = update.message.reply_to_message.from_user['id']
-    if update.message.reply_to_message.from_user['username']:
-        username = "@" + update.message.reply_to_message.from_user['username']
+    message_text = update.message.text
+    message_text = message_text.split(' ')
+
+    if message_text[1]:
+        # Command with param - @username
+        logger.info(message_text)
+        bakchod = bakchod_util.get_bakchod_with_username(message_text[1])
     else:
-        username = update.message.reply_to_message.from_user['first_name']
+        # Command without param
+        user_id = update.message.reply_to_message.from_user['id']
+        bakchod = bakchod_util.get_bakchod(user_id)
 
-    a_bakchod = bakchod_util.get_bakchod(user_id)
+    return bakchod
+
+
+def process_user(bakchod, to_censor):
 
     if to_censor:
-        a_bakchod.censored = True
-        logger.info("/censor: request for %s", username)
-        response = "☢️ Censoring " + username + "!"
+        bakchod.censored = True
+        logger.info("/censor: request for %s", bakchod.username)
+        response = "☢️ Censoring " + bakchod.username + "!"
     else:
-        a_bakchod.censored = False
-        logger.info("/uncensor: request for %s", username)
-        response = "☢️ Uncensoring " + username + "!"
+        bakchod.censored = False
+        logger.info("/uncensor: request for %s", bakchod.username)
+        response = "☢️ Uncensoring " + bakchod.username + "!"
 
-    bakchod_util.set_bakchod(a_bakchod)
+    bakchod_util.set_bakchod(bakchod)
 
     return response
-
-
-def is_admin(og_bakchod):
-    if str(og_bakchod) in config.allowed_setters:
-        logger.info("is_admin: og_bakchod in config.allowed_setters")
-        return True
-    else:
-        return False
