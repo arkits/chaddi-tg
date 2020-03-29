6d80d8febf592437a48a2419834fc95500de6c56
Archit Khode <arkits@outlook.com>
2020-03-28 18:06:13 -0700

    add group related dao

--- src/util/dao.py
+++ src/util/dao.py
@@ -6,6 +6,30 @@ import traceback
 db_list = ["bakchods", "groups", "quotes"]
 
 
+def init_db():
+
+    logger.info("Initializing DBs...")
+
+    for db in db_list:
+
+        try:
+            with shelve.open("resources/db/" + db, "c") as shelf:
+
+                if shelf.get(db) is not None:
+                    logger.debug("db={} is already setup...", db)
+                else:
+                    logger.debug("Setting up db={}", db)
+                    shelf[db] = {}
+
+                shelf.close()
+        except Exception as e:
+            logger.error(
+                "Caught Error in dao.init_db - {} \n {}", e, traceback.format_exc()
+            )
+
+    return
+
+
 def update_bakchod(bakchod):
 
     try:
@@ -39,28 +63,37 @@ def get_bakchod(bakchod_id):
     return bakchod_to_return
 
 
-def init_db():
+def update_group(group):
 
-    logger.info("Initializing DBs...")
-
-    for db in db_list:
-
-        try:
-            with shelve.open("resources/db/" + db, "c") as shelf:
-
-                if shelf.get(db) is not None:
-                    logger.debug("db={} is already setup...", db)
-                else:
-                    logger.debug("Setting up db={}", db)
-                    shelf[db] = {}
-
-                shelf.close()
-        except Exception as e:
-            logger.error(
-                "Caught Error in dao.init_db - {} \n {}", e, traceback.format_exc()
-            )
+    try:
+        with shelve.open("resources/db/groups", "c") as shelf:
+            groups = shelf["groups"]
+            groups[group.id] = group
+            shelf["groups"] = groups
+            shelf.close()
+    except Exception as e:
+        logger.error(
+            "Caught Error in dao.update_group - {} \n {}", e, traceback.format_exc()
+        )
 
     return
+
+
+def get_group(group_id):
+
+    group_to_return = None
+
+    try:
+        with shelve.open("resources/db/groups", "r") as shelf:
+            groups = shelf.get("groups")
+            group_to_return = groups.get(group_id)
+            shelf.close()
+    except Exception as e:
+        logger.error(
+            "Caught Error in dao.get_group - {} \n {}", e, traceback.format_exc()
+        )
+
+    return group_to_return
 
 
 # Initialize DBs on startup
