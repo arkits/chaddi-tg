c383299865fadd1d1d8f3e8ee6ba11be126124d1
Archit Khode <arkits@outlook.com>
2020-04-07 17:19:59 -0700

    make log_chat more safer

--- src/util/util.py
+++ src/util/util.py
@@ -20,12 +20,29 @@ def print_title():
 
 
 def log_chat(handler_name, update):
-    logger.info(
-        "[{}] Handling request from user '{}' in group '{}'",
-        handler_name,
-        update.message.from_user["username"],
-        update.message.chat.title,
-    )
+
+    pretty_name = "None"
+
+    try:
+        if update.message.from_user["username"] is not None:
+            pretty_name = update.message.from_user["username"]
+        elif update.message.from_user["first_name"] is not None:
+            pretty_name = update.message.from_user["first_name"]
+    except Exception as e:
+        pass
+
+    try:
+        logger.info(
+            "[{}] Handling request from user '{}' in group '{}'",
+            handler_name,
+            pretty_name,
+            update.message.chat.title,
+        )
+    except Exception as e:
+        # This is by design... logging shouldn't throw an Error, 
+        # only my terrible coding should!
+        pass
+
 
 
 def is_admin(og_bakchod):
