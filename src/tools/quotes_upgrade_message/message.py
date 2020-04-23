e133937c27698088e6aa3638860763fe44b01204
Archit Khode <arkits@outlook.com>
2020-04-22 20:33:17 -0700

    document get_quote_by_id_legacy

--- src/tools/quotes_upgrade_message/message.py
+++ src/tools/quotes_upgrade_message/message.py
@@ -26,3 +26,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# def get_quote_by_id_legacy(quote_id):
+# 
+#     quote = None
+# 
+#     try:
+#         c.execute("""SELECT * FROM quotes WHERE id=:id""", {"id": quote_id})
+#         query_result = c.fetchone()
+# 
+#         if query_result is not None:
+#             quote = {}
+#             quote["id"] = query_result[0]
+#             quote["message"] = query_result[1]
+#             quote["user"] = query_result[2]
+#             quote["date"] = query_result[3]
+# 
+#     except Exception as e:
+#         logger.error(
+#             "Caught Error in dao.get_quote_by_id - {} \n {}", e, traceback.format_exc(),
+#         )
+# 
+#     return quote
