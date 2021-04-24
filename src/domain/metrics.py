853d71b3fa6cc2db77a3a871ee19e537dc9df2ee
Archit Khode <arkits@outlook.com>
2021-04-24 11:16:37 -0700

    metrics: added command usage counter

--- src/domain/metrics.py
+++ src/domain/metrics.py
@@ -6,9 +6,22 @@ message_counter = Counter(
     "chaddi_messages_count", "chaddi_messages_count", ["group_id", "group_name"]
 )
 
+command_usage_counter = Counter(
+    "chaddi_command_usage_count",
+    "chaddi_command_usage_count",
+    ["group_id", "group_name"],
+)
+
 
 def inc_message_count(update: Update):
     message_counter.labels(
+        group_id=update.message.chat.id, group_name=update.message.chat.title
+    ).inc()
+    return
+
+
+def inc_command_usage_count(update: Update):
+    command_usage_counter.labels(
         group_id=update.message.chat.id, group_name=update.message.chat.title
     ).inc()
     return
