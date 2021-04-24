f3d80d7964b9bb2520f286843b2573301dded20b
Archit Khode <arkits@outlook.com>
2021-04-24 10:15:06 -0700

    dev: use ParseMode.HTML

--- src/bot/handlers/about.py
+++ src/bot/handlers/about.py
@@ -1,4 +1,5 @@
 from telegram import Update
+from telegram.parsemode import ParseMode
 from src.domain import dc, util
 from src.db import Bakchod, bakchod
 
@@ -14,15 +15,19 @@ def handle(update: Update, context):
     else:
         b = bakchod.get_or_create_bakchod_from_tg_user(update.message.from_user)
 
-    update.message.reply_text(text=generate_about_response(b))
+    update.message.reply_text(
+        text=generate_about_response(b), parse_mode=ParseMode.HTML
+    )
 
 
 def generate_about_response(bakchod: Bakchod):
 
     about_response = (
-        "*About " + util.extract_pretty_name_from_bakchod(bakchod) + ":* \n"
+        "<b>About " + util.extract_pretty_name_from_bakchod(bakchod) + ":</b> \n"
     )
-    about_response = about_response + "~ ID: `{}` \n".format(bakchod.tg_id)
-    about_response = about_response + "~ ₹okda: `{}` \n".format(round(bakchod.rokda, 2))
+    about_response = about_response + "~ ID: <code>{}</code> \n".format(bakchod.tg_id)
+    about_response = about_response + "~ ₹okda: <code>{}</code> \n".format(
+        round(bakchod.rokda, 2)
+    )
 
     return about_response
