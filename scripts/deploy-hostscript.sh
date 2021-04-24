8e29471511aa0c8537577543db36067b6f97c3ca
Archit Khode <arkits@outlook.com>
2021-04-24 00:06:12 -0700

    ci: improve scripts ux

--- scripts/deploy-hostscript.sh
+++ scripts/deploy-hostscript.sh
@@ -5,17 +5,32 @@ set -e
 
 echo ">>> hostscript running as $(whoami)@$(hostname)"
 echo ">>> deploying chaddi-tg to $(pwd)"
+echo ""
 
 if [ -d "chaddi-tg" ]; then
+
     echo ">>> chaddi-tg was present"
     cd chaddi-tg
+    echo ""
+
+    echo ">>> git pulling latest from remote"
     git pull
-    cd scripts
+    echo ""
+
     echo ">>> triggering kill-chaddi.sh"
+    cd scripts
     ./kill-chaddi.sh
+
     echo ">>> starting new chaddi"
     ./run-prod.sh
+    echo ""
+
+    sleep 5
+    echo ">>> checking processes"
+    ps aux | grep "python chaddi.py"
+
 else
+
     echo ">>> chaddi-tg was not present!"
     exit 4
 fi
--- scripts/kill-chaddi.sh
+++ scripts/kill-chaddi.sh
@@ -4,9 +4,10 @@
 PID=$(ps -eaf | grep "python chaddi.py" | grep -v grep | awk '{print $2}')
 
 if [[ "" != "$PID" ]]; then
-    echo ">>> Killing PID - $PID"
+    echo ">>> killing PID - $PID"
     kill -9 $PID
 fi
 
 echo ">>> checking processes"
 ps aux | grep "python chaddi.py"
+echo ""
