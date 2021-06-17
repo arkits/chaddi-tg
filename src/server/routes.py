df90c822ef94d9bc74997e47da1e10f5c1cf526a
Archit Khode <arkits@outlook.com>
2021-06-16 19:29:00 -0700

    dev: initial impl to set metadata from UI

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Request
+import json
+from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.db import Bakchod, Group, Message, Quote, group
@@ -43,6 +44,7 @@ async def get_groups(request: Request):
     return templates.TemplateResponse(
         "messages.html", {"request": request, "messages": messages}
     )
+
 
 @router.get("/details/bakchod", response_class=HTMLResponse)
 async def get_details_group(request: Request, tg_id: str = "unset"):
@@ -105,4 +107,61 @@ async def get_groups(request: Request):
 
     return templates.TemplateResponse(
         "quotes.html", {"request": request, "quotes": quotes}
+    )
+
+
+@router.post("/api/bakchod/metadata", response_class=HTMLResponse)
+async def post_update_bakchod_metadata(
+    request: Request, metadata: str = Form("unset"), tg_id: str = Form("unset")
+):
+
+    logger.info("post_update_bakchod_metadata tg_id={} metadata={}", tg_id, metadata)
+
+    response_message = {
+        "title": "Success",
+        "message": "Metadata updated successfully",
+        "alert_type": "alert-success",  # alert-success, alert-danger
+    }
+
+    try:
+
+        if tg_id == "unset":
+            raise Exception("tg_id was unset")
+
+        if metadata == "unset":
+            raise Exception("metadata was unset")
+
+        # Check if metadata is a JSON
+        try:
+            json.loads(metadata)
+        except Exception as e:
+            raise Exception("Error during json.loads(metadata) :: " + str(e))
+
+        b = Bakchod.get_by_id(tg_id)
+        if b is None:
+            raise Exception("Unable to find Bakchod")
+
+        b.metadata = metadata
+        b.save()
+
+    except Exception as e:
+
+        logger.error("Caught Exception - e={}", e)
+
+        response_message["title"] = "Backend Error"
+        response_message["message"] = e
+        response_message["alert_type"] = "alert-danger"
+
+        return templates.TemplateResponse(
+            "details_bakchod.html",
+            {
+                "request": request,
+                "bakchod": None,
+                "response_message": response_message,
+            },
+        )
+
+    return templates.TemplateResponse(
+        "details_bakchod.html",
+        {"request": request, "bakchod": b, "response_message": response_message},
     )
