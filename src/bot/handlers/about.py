2a1c87c8754770206b2edda6cb83ea484dff489f
Archit Khode <arkits@outlook.com>
2022-03-28 19:27:29 -0700

    about: add search and expose lastseen

--- src/bot/handlers/about.py
+++ src/bot/handlers/about.py
@@ -1,23 +1,51 @@
-from telegram import Update
+from telegram.ext import CallbackContext
+from telegram import Update, Message
+from loguru import logger
 from telegram.parsemode import ParseMode
 from src.domain import dc, util
 from src.db import Bakchod, bakchod_dao
 
 
-def handle(update: Update, context):
+def handle(update: Update, context: CallbackContext):
 
-    dc.log_command_usage("about", update)
+    try:
 
-    if update.message.reply_to_message:
-        b = bakchod_dao.get_or_create_bakchod_from_tg_user(
-            update.message.reply_to_message.from_user
+        dc.log_command_usage("about", update)
+
+        username = parse_username(update.message.text)
+        logger.info("[about] username='{}'", username)
+
+        if username is None:
+
+            # handle based on context
+            if update.message.reply_to_message:
+                b = bakchod_dao.get_or_create_bakchod_from_tg_user(
+                    update.message.reply_to_message.from_user
+                )
+            else:
+                b = bakchod_dao.get_or_create_bakchod_from_tg_user(
+                    update.message.from_user
+                )
+
+        else:
+
+            # search for bakchod by username
+            b = bakchod_dao.get_bakchod_by_username(username)
+            if b is None:
+                update.message.reply_text(
+                    "Kaun hai bee '{}'??? Try <code>/about @username</code>".format(
+                        username
+                    ),
+                    parse_mode=ParseMode.HTML
+                )
+                return
+
+        update.message.reply_text(
+            text=generate_about_response(b), parse_mode=ParseMode.HTML
         )
-    else:
-        b = bakchod_dao.get_or_create_bakchod_from_tg_user(update.message.from_user)
 
-    update.message.reply_text(
-        text=generate_about_response(b), parse_mode=ParseMode.HTML
-    )
+    except Exception as e:
+        logger.error("Caught Exception in about.handle - e={}", e)
 
 
 def generate_about_response(bakchod: Bakchod):
@@ -29,5 +57,23 @@ def generate_about_response(bakchod: Bakchod):
     about_response = about_response + "~ ₹okda: <code>{}</code> \n".format(
         round(bakchod.rokda, 2)
     )
+    about_response = about_response + "~ Last seen: <code>{}</code> \n".format(
+        bakchod.lastseen
+    )
 
     return about_response
+
+
+def parse_username(message: str):
+
+    split_message = message.split(" ")
+
+    if not len(split_message) > 1:
+        return None
+
+    username = split_message[1]
+
+    if username.startswith("@"):
+        username = username[1:]
+
+    return username
