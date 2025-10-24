bdab5a17f185cfaf2b7228a39f2c55c49c6fbe46
Archit Khode <arkits@outlook.com>
2025-10-23 18:42:06 -0700

    Refactor reschedule_saved_jobs to optimize time calculation and enhance logging for job rescheduling status

--- src/domain/scheduler.py
+++ src/domain/scheduler.py
@@ -9,9 +9,29 @@ from src.db import ScheduledJob
 
 def reschedule_saved_jobs(job_queue):
     sjs = ScheduledJob.select()
+    current_time = int(time.time())
+    rescheduled_count = 0
+    skipped_count = 0
 
     for sj in sjs:
-        due_seconds = sj.job_context["reminder_time"] - int(time.time())
+        due_seconds = sj.job_context["reminder_time"] - current_time
+
+        # Skip jobs that are in the past
+        if due_seconds <= 0:
+            job_name = build_job_name(
+                sj.job_context["chat_id"],
+                sj.job_context["from_bakchod_id"],
+                sj.job_context["job_id"],
+            )
+            logger.warning(
+                "Skipping past job job_name={} due_seconds={} (was scheduled for the past)",
+                job_name,
+                due_seconds,
+            )
+            # Delete the old job from database
+            sj.delete_instance()
+            skipped_count += 1
+            continue
 
         job_name = build_job_name(
             sj.job_context["chat_id"],
@@ -27,5 +47,12 @@ def reschedule_saved_jobs(job_queue):
         )
 
         logger.info("Re-scheduled job_name={} job_context={}", job_name, sj.job_context)
+        rescheduled_count += 1
+
+    logger.info(
+        "Job rescheduling complete: rescheduled={} skipped={}",
+        rescheduled_count,
+        skipped_count,
+    )
 
     return
