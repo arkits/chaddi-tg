22247fa22c117d38b8c640b72efc7bc774aad841
Archit Khode <arkits@outlook.com>
2025-10-23 19:31:29 -0700

    Add process termination for server running on port 5100 in kill-chaddi.sh

--- scripts/kill-chaddi.sh
+++ scripts/kill-chaddi.sh
@@ -11,3 +11,14 @@ fi
 echo ">>> checking processes"
 ps aux | grep "python chaddi.py"
 echo ""
+
+# kill server running on port 5100
+PORT=5100
+PID_PORT=$(lsof -t -i:$PORT)
+if [[ "" != "$PID_PORT" ]]; then
+    echo ">>> killing process on port $PORT - PID $PID_PORT"
+    kill -9 $PID_PORT
+fi
+echo ">>> checking processes on port $PORT"
+lsof -i:$PORT
+echo ""
